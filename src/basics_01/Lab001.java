280b0511cefed78d7e126d16752e16ae3a3a6f4c
dhaval-harkhani <harkhanidhaval@gmail.com>
2024-01-01 15:08:54 +0530

    my first commit 1 jan

---
+++ src/basics_01/Lab001.java
@@ -0,0 +1,9 @@
+package src.basics_01;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello Dhaval");
+    }
+
+}
---
+++ src/basics_01/Lab002.java
@@ -0,0 +1,18 @@
+package src.basics_01;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+
+        byte age = 10; // range of byte -128 to 127
+        age = 127; // out of range
+        System.out.println(age);
+        //age = age + 1;
+        System.out.println(age);
+
+        short _age = 128; // -32768 to 32768
+        System.out.println(_age);
+
+
+    }
+}
